8d49de58e345f82d6140025608a3fafb1dea074e
Oleksii26 <Kolesnik26@ukr.net>
2022-12-22 21:59:10 +0200

    sve

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,12 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { authOperations } from 'redux/auth/authOperation'
-import { getIsLoggedIn, getUserName } from 'redux/auth/authSelectors'
+import { getUserName } from 'redux/auth/authSelectors'
 import css from './UserMenu.module.css'
 
 export const UserMenu = () => {
     const dispatch = useDispatch()
     const user = useSelector(getUserName)
-    const log = useSelector(getIsLoggedIn)
 
     return (
         <div>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store'
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
